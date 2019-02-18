8401755c351d60d290eb993e87c4d4f0001adbad
boontat.puah <boontat.puah@fujixerox.com>
2019-02-18 17:37:45 +0800

    boiletplate

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import Table from './Table';
+
+class App extends Component {
+    render() {
+        const characters = [
+            {
+                'name': 'Charlie',
+                'job': 'Janitor'
+            },
+            {
+                'name': 'Mac',
+                'job': 'Bouncer'
+            },
+            {
+                'name': 'Dee',
+                'job': 'Aspring actress'
+            },
+            {
+                'name': 'Dennis',
+                'job': 'Bartender'
+            },
+            {
+                'name': 'howdy',
+                'job': 'horsy'
+            }
+        ];
+
+        return (
+            <div className="container">
+                <Table characterData={characters} />
+            </div>
+        );
+    }
+}
+
+export default App;
