a8b840e52a9f5e41cb4863f0105c64d54ad4b766
boontat.puah <boontat.puah@fujixerox.com>
2019-02-18 17:59:03 +0800

    add state

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+## Tutorial from
+https://www.taniarascia.com/getting-started-with-react/
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, {Component} from 'react';
 import Table from './Table';
 
 class App extends Component {
-    render() {
-        const characters = [
+    state = {
+        characters: [
             {
                 'name': 'Charlie',
                 'job': 'Janitor'
@@ -24,11 +24,45 @@ class App extends Component {
                 'name': 'howdy',
                 'job': 'horsy'
             }
+        ]
+    };
+
+    removeCharacter = index => {
+        const { characters } = this.state;
+
+        this.setState({
+            characters: characters.filter((character, i) => {
+                return i !== index;
+            })
+        });
+    }
+
+    render() {
+        const characters = [
+            {
+                'name': 'Charlie',
+                'job': 'Janitor'
+            },
+            {
+                'name': 'Mac',
+                'job': 'Bouncer'
+            },
+            {
+                'name': 'Dee',
+                'job': 'Aspring actress'
+            },
+            {
+                'name': 'Dennis',
+                'job': 'Bartender'
+            }
         ];
 
         return (
             <div className="container">
-                <Table characterData={characters} />
+                <Table
+                    characterData={characters}
+                    removeCharacter={this.removeCharacter}
+                />
             </div>
         );
     }
