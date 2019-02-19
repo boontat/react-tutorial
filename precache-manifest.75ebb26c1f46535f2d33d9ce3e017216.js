83f847209a34f538fc9ce2a2035fe0d9618e547c
boontat.puah <boontat.puah@fujixerox.com>
2019-02-19 15:18:10 +0800

    Updates

---
+++ precache-manifest.75ebb26c1f46535f2d33d9ce3e017216.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b2d15347dbf2dd6be24",
+    "url": "/react-tutorial/static/js/runtime~main.9b2d1534.js"
+  },
+  {
+    "revision": "33831fbcc7e574f1be60",
+    "url": "/react-tutorial/static/js/main.33831fbc.chunk.js"
+  },
+  {
+    "revision": "62f3f2f65292d018eecc",
+    "url": "/react-tutorial/static/js/2.62f3f2f6.chunk.js"
+  },
+  {
+    "revision": "33831fbcc7e574f1be60",
+    "url": "/react-tutorial/static/css/main.68bf3795.chunk.css"
+  },
+  {
+    "revision": "f4e58b2d5ef8b5f49d8553578a2e9f43",
+    "url": "/react-tutorial/index.html"
+  }
+];
